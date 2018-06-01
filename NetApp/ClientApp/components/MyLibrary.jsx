e30e8b60b1cd95c7c698696ae3512fcd83a87101
wang peizhong <w.xy.z@live.com>
2018-06-02 00:24:30 +0800

    add jsx file

---
+++ NetApp/ClientApp/components/MyLibrary.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { connect } from "react-redux";
+
+class MyLibaray extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, World!</h1>
+        <div className="progress">
+          <div
+            className="progress-bar progress-bar-success progress-bar-striped active"
+            role="progressbar"
+            aria-valuenow="60"
+            aria-valuemin="0"
+            aria-valuemax="100"
+            style={{ width: "43%" }}
+          >
+            43%
+          </div>
+        </div>
+        <div className="list-group">
+          <a href="#" className="list-group-item">
+            Instagram
+          </a>
+          <a href="#" className="list-group-item">
+            WhatsApp
+          </a>
+          <a href="#" className="list-group-item">
+            Oculus
+          </a>
+        </div>
+        <button className="btn btn-primary pull-left">Look Good</button>
+      </div>
+    );
+  }
+}
+
+export default connect()(MyLibaray);
--- NetApp/ClientApp/routes.tsx
+++ NetApp/ClientApp/routes.tsx
@@ -1,12 +1,16 @@
-import * as React from 'react';
-import { Route } from 'react-router-dom';
-import { Layout } from './components/Layout';
-import Home from './components/Home';
-import FetchData from './components/FetchData';
-import Counter from './components/Counter';
+import * as React from "react";
+import { Route } from "react-router-dom";
+import { Layout } from "./components/Layout";
+import Home from "./components/Home";
+import FetchData from "./components/FetchData";
+import Counter from "./components/Counter";
+import MyLibrary from "./components/MyLibrary";
 
-export const routes = <Layout>
-    <Route exact path='/' component={ Home } />
-    <Route path='/counter' component={ Counter } />
-    <Route path='/fetchdata/:startDateIndex?' component={ FetchData } />
-</Layout>;
+export const routes = (
+  <Layout>
+    <Route exact path="/" component={Home} />
+    <Route path="/counter" component={Counter} />
+    <Route path="/fetchdata/:startDateIndex?" component={FetchData} />
+    <Route path="/mylibrary" component={MyLibrary} />
+  </Layout>
+);
