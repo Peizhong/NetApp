904020c01e829fa1f77815607d01de36e87fff34
wangpeizhong <wangpeizhong@9NPNT2X>
2018-06-02 15:07:03 +0800

    改改

--- NetApp/ClientApp/components/Counter.tsx
+++ NetApp/ClientApp/components/Counter.tsx
@@ -1,31 +1,50 @@
 import * as React from 'react';
 import { Link, RouteComponentProps } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { ApplicationState }  from '../store';
+import { ApplicationState } from '../store';
 import * as CounterStore from '../store/Counter';
 import * as WeatherForecasts from '../store/WeatherForecasts';
 
-type CounterProps =
-    CounterStore.CounterState
-    & typeof CounterStore.actionCreators
-    & RouteComponentProps<{}>;
+type CounterProps = CounterStore.CounterState &
+  typeof CounterStore.actionCreators &
+  RouteComponentProps<{}>;
 
 class Counter extends React.Component<CounterProps, {}> {
-    public render() {
-        return <div>
-            <h1>Counter</h1>
+  public render() {
+    return (
+      <div>
+        <h1>Counter</h1>
 
-            <p>This is a simple example of a React component.</p>
+        <p>This is a simple example of a React component.</p>
 
-            <p>Current count: <strong>{ this.props.count }</strong></p>
-
-            <button onClick={ () => { this.props.increment() } }>Increment</button>
-        </div>;
-    }
+        <p>
+          Current count: <strong>{this.props.count}</strong>
+        </p>
+        <div className="btn-group" role="group" aria-label="...">
+          <button
+            className="btn btn-default"
+            onClick={() => {
+              this.props.decrement();
+            }}
+          >
+            Decrement
+          </button>
+          <button
+            className="btn btn-default"
+            onClick={() => {
+              this.props.increment();
+            }}
+          >
+            Increment
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 // Wire up the React component to the Redux store
 export default connect(
-    (state: ApplicationState) => state.counter, // Selects which state properties are merged into the component's props
-    CounterStore.actionCreators                 // Selects which action creators are merged into the component's props
+  (state: ApplicationState) => state.counter, // Selects which state properties are merged into the component's props
+  CounterStore.actionCreators // Selects which action creators are merged into the component's props
 )(Counter) as typeof Counter;
