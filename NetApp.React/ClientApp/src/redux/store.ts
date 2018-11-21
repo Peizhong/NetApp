59f932f6fd1f2081ab2d88b97bb5586f9429c40c
Peizhong Wang <w.xy.z@live.com>
2018-11-21 08:55:55 +0800

    redux-logger

--- NetApp.React/ClientApp/src/redux/store.ts
+++ NetApp.React/ClientApp/src/redux/store.ts
@@ -1,5 +1,7 @@
 import { applyMiddleware, createStore } from "redux";
+// Logger with default options
+import logger from "redux-logger";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
-export default createStore(rootReducer, applyMiddleware(thunk));
+export default createStore(rootReducer, applyMiddleware(thunk, logger));
