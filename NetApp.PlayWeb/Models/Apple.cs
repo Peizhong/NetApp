0ad70ffd84e5fde5c7d252c3187b758796372feb
Peizhong Wang <w.xy.z@live.com>
2018-11-26 21:27:40 +0800

    add open source

---
+++ NetApp.PlayWeb/Models/Apple.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetApp.PlayWeb.Models
+{
+    public class Apple
+    {
+        [JsonIgnore]
+        public string Id { get; set; }
+
+        [JsonProperty("Name")]
+        public string Name { get; set; }
+
+        [JsonProperty("Value")]
+        public string Value { get; set; }
+    }
+}
